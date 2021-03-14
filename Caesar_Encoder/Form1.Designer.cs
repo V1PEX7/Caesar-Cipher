26f362bc9ff0a0702ff013f2d4b1d47385efad64
V1PEX7 <micbel2000@gmail.com>
2021-03-14 18:24:52 +0300

    Update Form1.Designer.cs

--- Caesar_Encoder/Form1.Designer.cs
+++ Caesar_Encoder/Form1.Designer.cs
@@ -51,24 +51,24 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.Size = new System.Drawing.Size(28, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Текст";
+            this.label1.Text = "Text";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(15, 208);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.Size = new System.Drawing.Size(28, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Сдвиг";
+            this.label2.Text = "Shift";
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(15, 224);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(55, 20);
+            this.textBox2.Size = new System.Drawing.Size(95, 20);
             this.textBox2.TabIndex = 3;
             // 
             // button1
@@ -77,7 +77,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "Шифровать";
+            this.button1.Text = "Encode";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -87,7 +87,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "Дешифровать";
+            this.button2.Text = "Decode";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -104,9 +104,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(15, 276);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.Size = new System.Drawing.Size(37, 13);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Результат";
+            this.label3.Text = "Result";
             // 
             // Form1
             // 
@@ -122,7 +122,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Caesar cipher";
             this.ResumeLayout(false);
             this.PerformLayout();
 
